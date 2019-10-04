673d8303ced706ac7b546c2949e196dbf94d34d9
aditya reddy <adityareddy561@gmail.com>
2019-10-04 18:45:58 +0530

    Update ramu.py

--- ramu.py
+++ ramu.py
@@ -1,6 +1,8 @@
 n=int(input())
 for i in range(int(n**(1/3))):
     for j in range(int(n**(1/3))):
-                if i!=j :
-                    print(i**3+j**3)
+              print(i**3+j**3 if i!=j)
+                   
+                    
+              
                     
